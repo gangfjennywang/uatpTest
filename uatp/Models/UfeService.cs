e0237e54859dde58cb2698582aa670e8bd2e3065
Jenny Wang <gangfwang@gmail.com>
2024-09-13 13:45:13 -0400

    uatp project controller

---
+++ uatp/Models/UfeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class UfeService
+{
+    private  static readonly Lazy<UfeService>  _instance = new Lazy<UfeService>(() => new UfeService());
+    public static UfeService Instance => _instance.Value;
+
+    private decimal _currentFeeMultiplier;
+    private DateTime _lastUpdate;
+
+    private UfeService()
+    {
+        _currentFeeMultiplier = 1m; // Initial fee multiplier
+        _lastUpdate = DateTime.UtcNow;
+        UpdateFeeMultiplier();
+    }
+
+    public decimal GetFeeMultiplier()
+    {
+        if ((DateTime.UtcNow - _lastUpdate).TotalHours >= 1)
+        {
+            UpdateFeeMultiplier();
+        }
+
+        return _currentFeeMultiplier;
+    }
+
+    private void UpdateFeeMultiplier()
+    {
+        var random = new Random();
+        _currentFeeMultiplier = (decimal)random.NextDouble() * 2; // Random decimal between 0 and 2
+        _lastUpdate = DateTime.UtcNow;
+    }
+}
